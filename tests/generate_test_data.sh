a07319824377dadf54a6e0a20fb954beb9b3fbd8
ignasrum <rumbavicius@gmail.com>
2021-10-03 18:50:33 +0200

    test data

--- tests/generate_test_data.sh
+++ tests/generate_test_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DIR="${0%/*}"
+
 file_exists () {
 	if [ -f "$1" ]; then
 		echo $1
@@ -18,15 +20,23 @@ dir_exists () {
 	fi
 }
 
-if ! file_exists "SRR3733117.1.fastq" ; then
+TEST_DATA="test_data/"
+
+if ! dir_exists $TEST_DATA ; then
+	mkdir test_data;
+fi
+
+if ! file_exists $TEST_DATA"SRR3733117.1.fastq" ; then
 	wget -nc https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos1/sra-pub-run-1/SRR3733117/SRR3733117.1;
 	fasterq-dump SRR3733117.1;
 	rm SRR3733117.1;
+	mv SRR3733117.1.fastq $TEST_DATA
 fi
 
-if ! dir_exists "bowtie2" ; then
+if ! dir_exists $TEST_DATA"bowtie2" ; then
 	wget -nc https://genome-idx.s3.amazonaws.com/bt/grch38_1kgmaj_snvindels_bt2.zip;
 	mkdir bowtie2;
 	unzip grch38_1kgmaj_snvindels_bt2.zip -d bowtie2;
 	rm -f grch38_1kgmaj_snvindels_bt2.zip;
+	mv bowtie2 $TEST_DATA
 fi
