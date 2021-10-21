eb05e230cbd82231d7a7b9d7bf76b845697abd3a
ignasrum <rumbavicius@gmail.com>
2021-10-21 14:51:20 +0200

    Added Bowtie2Bowtie2 pipeline

--- hocort/pipelines/__init__.py
+++ hocort/pipelines/__init__.py
@@ -1,4 +1,5 @@
 from hocort.pipelines.bowtie2 import Bowtie2
 from hocort.pipelines.bowtie2_hisat2 import Bowtie2HISAT2
+from hocort.pipelines.bowtie2_bowtie2 import Bowtie2Bowtie2
 from hocort.pipelines.bwa_mem2 import BWA_MEM2
 from hocort.pipelines.hisat2 import HISAT2
