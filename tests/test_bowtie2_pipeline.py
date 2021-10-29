d56e092755e0f6c86eaeccdd792d664c5ec9ed08
ignasrum <rumbavicius@gmail.com>
2021-10-29 19:57:27 +0200

    Added tests for hcfilter

--- tests/test_bowtie2_pipeline.py
+++ tests/test_bowtie2_pipeline.py
@@ -11,6 +11,22 @@ out1 = f'{temp_dir.name}/out1.fastq'
 seq2 = f'{path}/test_data/sequences/sequences2.fastq'
 out2 = f'{temp_dir.name}/out2.fastq'
 
+def test_bowtie2_pipeline_hcfilter_true_1():
+    returncode, stdout, stderr = Bowtie2().run(idx, seq1, out1, hcfilter='t')
+    assert returncode == 0
+
+def test_bowtie2_pipeline_hcfilter_false_1():
+    returncode, stdout, stderr = Bowtie2().run(idx, seq1, out1, hcfilter='f')
+    assert returncode == 0
+
+def test_bowtie2_pipeline_hcfilter_true_2():
+    returncode, stdout, stderr = Bowtie2().run(idx, seq1, out1, seq2=seq2, out2=out2, hcfilter='t')
+    assert returncode == 0
+
+def test_bowtie2_pipeline_hcfilter_false_2():
+    returncode, stdout, stderr = Bowtie2().run(idx, seq1, out1, seq2=seq2, out2=out2, hcfilter='f')
+    assert returncode == 0
+
 def test_bowtie2_pipeline_end_to_end_1():
     mode = 'end-to-end'
     returncode, stdout, stderr = Bowtie2().run(idx, seq1, out1, mode=mode)
--- tests/test_hisat2_pipeline.py
+++ tests/test_hisat2_pipeline.py
@@ -11,22 +11,38 @@ out1 = f'{temp_dir.name}/out1.fastq'
 seq2 = f'{path}/test_data/sequences/sequences2.fastq'
 out2 = f'{temp_dir.name}/out2.fastq'
 
-def test_bowtie2_pipeline_end_to_end_1():
+def test_hisat2_pipeline_hcfilter_true_1():
+    returncode, stdout, stderr = HISAT2().run(idx, seq1, out1, hcfilter='t')
+    assert returncode == 0
+
+def test_hisat2_pipeline_hcfilter_false_1():
+    returncode, stdout, stderr = HISAT2().run(idx, seq1, out1, hcfilter='f')
+    assert returncode == 0
+
+def test_hisat2_pipeline_hcfilter_true_2():
+    returncode, stdout, stderr = HISAT2().run(idx, seq1, out1, seq2=seq2, out2=out2, hcfilter='t')
+    assert returncode == 0
+
+def test_hisat2_pipeline_hcfilter_false_2():
+    returncode, stdout, stderr = HISAT2().run(idx, seq1, out1, seq2=seq2, out2=out2, hcfilter='f')
+    assert returncode == 0
+
+def test_hisat2_pipeline_end_to_end_1():
     options = []
     returncode, stdout, stderr = HISAT2().run(idx, seq1, out1, options=options)
     assert returncode == 0
 
-def test_bowtie2_pipeline_end_to_end_2():
+def test_hisat2_pipeline_end_to_end_2():
     options = []
     returncode, stdout, stderr = HISAT2().run(idx, seq1, out1, seq2=seq2, out2=out2, options=options)
     assert returncode == 0
 
-def test_bowtie2_pipeline_local_1():
+def test_hisat2_pipeline_local_1():
     options = []
     returncode, stdout, stderr = HISAT2().run(idx, seq1, out1, options=options)
     assert returncode == 0
 
-def test_bowtie2_pipeline_local_2():
+def test_hisat2_pipeline_local_2():
     options = []
     returncode, stdout, stderr = HISAT2().run(idx, seq1, out1, seq2=seq2, out2=out2, options=options)
     assert returncode == 0
