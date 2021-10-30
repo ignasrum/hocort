1954dc06896bb20f660a31b88e4b6bb1efc5b738
ignasrum <rumbavicius@gmail.com>
2021-10-30 15:36:37 +0200

    updated aligner tests

--- tests/test_bowtie2_aligner.py
+++ tests/test_bowtie2_aligner.py
@@ -9,6 +9,27 @@ idx = f'{path}/test_data/bowtie2/genome'
 seq1 = f'{path}/test_data/sequences/sequences1.fastq'
 output = f'{temp_dir.name}/output'
 seq2 = f'{path}/test_data/sequences/sequences2.fastq'
+no_path = ''
+
+def test_idx_no_path():
+    returncode, stdout, stderr = bt2.align_sam(no_path, seq1, output)
+    assert returncode[0] == 255
+
+def test_seq1_no_path():
+    returncode, stdout, stderr = bt2.align_sam(idx, no_path, output)
+    assert returncode[0] == 1
+
+def test_output_no_path():
+    returncode, stdout, stderr = bt2.align_sam(idx, seq1, no_path)
+    assert returncode[0] == 0
+
+def test_seq1_seq2_no_path():
+    returncode, stdout, stderr = bt2.align_sam(idx, no_path, output, seq2=no_path)
+    assert returncode[0] == 0
+
+def test_seq2_no_path():
+    returncode, stdout, stderr = bt2.align_sam(idx, seq1, output, seq2=no_path)
+    assert returncode[0] == 1
 
 def test_local_sam_1():
     options = ['--local']
--- tests/test_hisat2_aligner.py
+++ tests/test_hisat2_aligner.py
@@ -9,25 +9,42 @@ idx = f'{path}/test_data/hisat2/genome'
 seq1 = f'{path}/test_data/sequences/sequences1.fastq'
 output = f'{temp_dir.name}/output'
 seq2 = f'{path}/test_data/sequences/sequences2.fastq'
+no_path = ''
+
+def test_idx_no_path():
+    returncode, stdout, stderr = hs2.align_sam(no_path, seq1, output)
+    assert returncode[0] == 255
+
+def test_seq1_no_path():
+    returncode, stdout, stderr = hs2.align_sam(idx, no_path, output)
+    assert returncode[0] == 1
+
+def test_output_no_path():
+    returncode, stdout, stderr = hs2.align_sam(idx, seq1, no_path)
+    assert returncode[0] == 1
+
+def test_seq1_seq2_no_path():
+    returncode, stdout, stderr = hs2.align_sam(idx, no_path, output, seq2=no_path)
+    assert returncode[0] == 1
+
+def test_seq2_no_path():
+    returncode, stdout, stderr = hs2.align_sam(idx, seq1, output, seq2=no_path)
+    assert returncode[0] == 1
 
 def test_sam_1():
-    options = []
-    returncode, stdout, stderr = hs2.align_sam(idx, seq1, output, options=options)
+    returncode, stdout, stderr = hs2.align_sam(idx, seq1, output)
     assert returncode[0] == 0
 
 def test_sam_2():
-    options = []
-    returncode, stdout, stderr = hs2.align_sam(idx, seq1, output, seq2=seq2, options=options)
+    returncode, stdout, stderr = hs2.align_sam(idx, seq1, output, seq2=seq2)
     assert returncode[0] == 0
 
 def test_bam_1():
-    options = []
-    returncode, stdout, stderr = hs2.align_bam(idx, seq1, output, options=options)
+    returncode, stdout, stderr = hs2.align_bam(idx, seq1, output)
     assert returncode[0] == 0
     assert returncode[1] == 0
 
 def test_bam_2():
-    options = []
-    returncode, stdout, stderr = hs2.align_bam(idx, seq1, output, seq2=seq2, options=options)
+    returncode, stdout, stderr = hs2.align_bam(idx, seq1, output, seq2=seq2)
     assert returncode[0] == 0
     assert returncode[1] == 0
