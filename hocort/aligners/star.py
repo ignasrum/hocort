1e2f2bbcb373ca49ca5bd398167c4938596ba4f8
ignasrum <rumbavicius@gmail.com>
2021-11-04 16:27:02 +0100

    implemented STAR aligner

---
+++ hocort/aligners/star.py
@@ -0,0 +1,25 @@
+import hocort.execute as exe
+import multiprocessing
+
+from hocort.aligners.aligner import Aligner
+
+
+class STAR(Aligner):
+    def generate_index(path, sequences):
+        pass
+
+    def align_sam(index, seq1, output, seq2=None, threads=multiprocessing.cpu_count(), options=[]):
+        cmd = ['STAR', '--runThreadN', str(threads), '--genomeDir', index, '--outFileNamePrefix', output] + options
+        cmd += ['--readFilesIn', seq1]
+        if seq2 is not None:
+            cmd += [seq2]
+
+        return exe.execute(cmd, decode_stderr=True)
+
+    def align_bam(index, seq1, output, seq2=None, threads=multiprocessing.cpu_count(), options=[]):
+        cmd = ['STAR', '--runThreadN', str(threads), '--genomeDir', index, '--outFileNamePrefix', output, '--outSAMtype BAM Unsorted'] + options
+        cmd += ['--readFilesIn', seq1]
+        if seq2 is not None:
+            cmd += [seq2]
+
+        return exe.execute(cmd, decode_stderr=True)
---
+++ tests/test_star_aligner.py
@@ -0,0 +1,57 @@
+from hocort.aligners.star import STAR
+import tempfile
+import os
+
+temp_dir = tempfile.TemporaryDirectory()
+path = os.path.dirname(__file__)
+
+idx = f'{path}/test_data/star'
+seq1 = f'{path}/test_data/sequences/sequences1.fastq'
+output = f'{temp_dir.name}/output'
+seq2 = f'{path}/test_data/sequences/sequences2.fastq'
+no_path = ''
+
+def test_idx_no_path():
+    returncode, stdout, stderr = STAR.align_sam(no_path, seq1, output)
+    print(stderr[0])
+    assert returncode[0] == 102
+
+def test_seq1_no_path():
+    returncode, stdout, stderr = STAR.align_sam(idx, no_path, output)
+    print(stderr[0])
+    assert returncode[0] == 102
+
+def test_output_no_path():
+    returncode, stdout, stderr = STAR.align_sam(idx, seq1, no_path)
+    print(stderr[0])
+    assert returncode[0] == 102
+
+def test_seq1_seq2_no_path():
+    returncode, stdout, stderr = STAR.align_sam(idx, no_path, output, seq2=no_path)
+    print(stderr[0])
+    assert returncode[0] == 102
+
+def test_seq2_no_path():
+    returncode, stdout, stderr = STAR.align_sam(idx, seq1, output, seq2=no_path)
+    print(stderr[0])
+    assert returncode[0] == 0
+
+def test_sam_1():
+    returncode, stdout, stderr = STAR.align_sam(idx, seq1, output)
+    print(stderr[0])
+    assert returncode[0] == 0
+
+def test_sam_2():
+    returncode, stdout, stderr = STAR.align_sam(idx, seq1, output, seq2=seq2)
+    print(stderr[0])
+    assert returncode[0] == 0
+
+def test_bam_1():
+    returncode, stdout, stderr = STAR.align_bam(idx, seq1, output)
+    print(stderr[0])
+    assert returncode[0] == 0
+
+def test_bam_2():
+    returncode, stdout, stderr = STAR.align_bam(idx, seq1, output, seq2=seq2)
+    print(stderr[0])
+    assert returncode[0] == 0
