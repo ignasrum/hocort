1e917add9f901c0522002d6a50e552c3fdc937fa
ignasrum <rumbavicius@gmail.com>
2021-11-06 22:13:52 +0100

    dependencies

---
+++ setup.sh
@@ -0,0 +1,5 @@
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda install bowtie2 minimap2 bwa-mem2 hisat2 pysam samtools bedtools seqtk htslib sra-tools vsearch
+conda install -c agbiome bbtools insilicoseq seqkit art fastx_toolkit emboss sga kraken2 cutadapt mummer4 recentrifuge star centrifuge
