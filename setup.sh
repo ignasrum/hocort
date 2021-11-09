363dde6292b8b65bf1410333c4171527f39353a3
ignasrum <rumbavicius@gmail.com>
2021-11-09 02:25:18 +0100

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@ conda config --add channels defaults
 conda config --add channels bioconda
 conda config --add channels conda-forge
 conda install seqtk sra-tools vsearch insilicoseq art htslib seqkit fastx_toolkit emboss sga cutadapt mummer4 recentrifuge
-conda install -c agbiome bbtools
 
 conda install -c pysam samtools bowtie2 minimap2 bwa-mem2 hisat2 kraken2 star bbmap #centrifuge
