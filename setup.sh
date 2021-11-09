55f1a8e8bd62f4ff45c0fe88a6cb994ee81f7264
ignasrum <rumbavicius@gmail.com>
2021-11-09 20:35:04 +0100

    dependencies

--- setup.sh
+++ setup.sh
@@ -1,5 +1,3 @@
-conda config --add channels defaults
 conda config --add channels bioconda
-#conda config --add channels conda-forge
 
 conda install -c bioconda pysam samtools bowtie2 minimap2 bwa-mem2 hisat2 kraken2 star bbmap #centrifuge
