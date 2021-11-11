5fd7b37a9148c06dbb8ceb86c74b2e4617597225
ignasrum <rumbavicius@gmail.com>
2021-11-11 22:41:36 +0000

    dependencies

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 conda config --add channels conda-forge 
 conda config --add channels bioconda
+conda config --set channel_priority flexible 
 
 conda install -c bioconda pysam samtools bowtie2 minimap2 bwa-mem2 hisat2 kraken2 star bbmap
 
--- setup1.sh
+++ setup1.sh
@@ -1,5 +1,6 @@
 conda config --add channels conda-forge 
 conda config --add channels bioconda
+conda config --set channel_priority flexible 
 
 conda install -c bioconda pysam
 conda install -c bioconda samtools
