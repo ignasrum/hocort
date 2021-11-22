a128efe2d65129af04e23a6f63e29a91d4dd10c8
ignasrum <rumbavicius@gmail.com>
2021-11-22 23:54:08 +0100

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,4 @@ conda config --add channels conda-forge
 conda config --add channels bioconda
 conda config --set channel_priority flexible 
 
-conda install -c bioconda pysam samtools bowtie2 minimap2 bwa-mem2 hisat2 kraken2 star bbmap
-
-# after install, deactivate then activate the environment
+conda install -c bioconda pysam samtools bowtie2 minimap2 bwa-mem2 hisat2 star bbmap kraken2
