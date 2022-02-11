e449d137ea2358b57c84d690f4959345f5d95d6c
ignasrum <rumbavicius@gmail.com>
2022-02-11 00:55:13 +0000

    updated helper.py

--- tests/helper.py
+++ tests/helper.py
@@ -2,5 +2,6 @@ import hocort.execute as exe
 
 def helper(cmd, expected, output=False):
     returncodes = exe.execute(cmd)
+    print(f'Returncodes: {returncodes}')
     for returncode in returncodes:
         assert returncode == expected
