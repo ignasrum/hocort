e14d16a75797f50786307a1de99d62e5b99692be
ignasrum <rumbavicius@gmail.com>
2022-03-17 15:14:04 +0100

    bump version to 0.3.1

--- hocort/version.py
+++ hocort/version.py
@@ -2,5 +2,5 @@
 A file for storing version and last_modified variables.
 
 """
-__version__ = '0.3.0'
-__last_modified__ = '23 February 2022'
+__version__ = '0.3.1'
+__last_modified__ = '17 March 2022'
