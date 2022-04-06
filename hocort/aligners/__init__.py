c764b38662b61b4f33073e52fddd110cda1f9d14
ignasrum <rumbavicius@gmail.com>
2022-04-06 15:07:57 +0200

    renamed bwa_mem2 to bwamem2

--- hocort/aligners/__init__.py
+++ hocort/aligners/__init__.py
@@ -2,4 +2,4 @@ from hocort.aligners.bbmap import BBMap as bbmap
 from hocort.aligners.bowtie2 import Bowtie2 as bowtie2
 from hocort.aligners.hisat2 import HISAT2 as hisat2
 from hocort.aligners.minimap2 import Minimap2 as minimap2
-from hocort.aligners.bwa_mem2 import BWA_MEM2 as bwa_mem2
+from hocort.aligners.bwa_mem2 import BWA_MEM2 as bwamem2
